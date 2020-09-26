52d3dd10a5b4012007bdf77dcef9402fc46e4b1d
Orozco David <dvdizcky@gmail.com>
2020-09-25 23:24:21 -0500

    Models: Remove empty values from marshall json

--- app/models/task.go
+++ app/models/task.go
@@ -1,8 +1,8 @@
 package models
 
 type Task struct {
-	ID            string        `json:"id"`
-	Type          string        `json:"type"`
-	Title         string        `json:"title"`
-	ResultChecker ResultChecker `json:"result_checker"`
+	ID            string         `json:"id,omitempty"`
+	Type          string         `json:"type,omitempty"`
+	Title         string         `json:"title,omitempty"`
+	ResultChecker *ResultChecker `json:"result_checker,omitempty"`
 }
