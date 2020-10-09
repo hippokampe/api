2785b2f766c5b25144db61203c67fc039e90de6a
Orozco David <dvdizcky@gmail.com>
2020-10-08 21:25:20 -0500

    models: task model updated to set the status

--- app/models/task.go
+++ app/models/task.go
@@ -4,5 +4,6 @@ type Task struct {
 	ID            string         `json:"id,omitempty"`
 	Type          string         `json:"type,omitempty"`
 	Title         string         `json:"title,omitempty"`
+	Done          bool           `json:"done,omitempty"`
 	ResultChecker *ResultChecker `json:"result_checker,omitempty"`
 }
