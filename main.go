10f91e0376214d5e54a3fbd9183e32faae4e66ee
Orozco David <dvdizcky@gmail.com>
2020-10-14 16:24:43 -0500

    main: switcher for select browser and setting variables on init func

--- main.go
+++ main.go
@@ -1,23 +1,60 @@
 package main
 
 import (
+	"log"
 	"os"
+	"strings"
 
-	"github.com/hippokampe/configuration"
+	"github.com/hippokampe/configuration/v2/configuration"
+	"github.com/hippokampe/configuration/v2/credentials"
 
 	"github.com/hippokampe/api/app/api"
 	"github.com/hippokampe/api/holberton"
 	"github.com/hippokampe/api/logger"
 )
 
-func main() {
-	config := configuration.New("/etc/hbtn/general.json")
-	if err := config.ReadGeneralConfig(); err != nil {
-		logger.Log2(err, "config generator")
-		os.Exit(1)
+var (
+	cred   *credentials.Credentials
+	config *configuration.InternalSettings
+)
+
+func browserSelector(browserPath string) string {
+	if strings.Contains(browserPath, "firefox") {
+		return holberton.FIREFOX
 	}
 
-	hbtn, err := holberton.NewSession(config.BrowserSelected, config)
+	if strings.Contains(browserPath, "chromium") {
+		return holberton.CHROMIUN
+	}
+
+	if strings.Contains(browserPath, "webkit") {
+		return holberton.WEBKIT
+	}
+
+	return ""
+}
+
+func init() {
+	config = configuration.New()
+	cred = credentials.New()
+
+	if err := config.BindCredentials(cred); err != nil {
+		log.Fatal(err)
+	}
+
+	config.SetFilename(os.Getenv("HIPPOKAMPE_CONFIGURATION"))
+	cred.SetFilename(os.Getenv("HIPPOKAMPE_CREDENTIALS"))
+
+	if err := config.ReadFromFile(); err != nil {
+		log.Fatal(err)
+	}
+
+	_ = cred.ReadFromFile()
+}
+
+func main() {
+	browserPath, _ := config.GetPathBrowser()
+	hbtn, err := holberton.NewSession(browserSelector(browserPath), config)
 	if err != nil {
 		logger.Log2(err, "could not create the session")
 
@@ -27,7 +64,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	api.New(hbtn, config)
+	if err := api.New(hbtn, config); err != nil {
+		if hbtn != nil {
+			hbtn.CloseSession()
+		}
+
+		log.Fatal(err)
+	}
 
 	hbtn.CloseSession()
 }
