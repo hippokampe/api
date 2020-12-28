89d69ffde8d9854874447bf54428869ae4dada99
Orozco David <dvdizcky@gmail.com>
2020-12-28 07:29:20 -0500

    holberton: login interface refactored

--- holberton/login.go
+++ holberton/login.go
@@ -1,77 +1,118 @@
 package holberton
 
 import (
+	"strings"
+
 	"github.com/gocolly/colly"
 	"github.com/hippokampe/api/app/models"
-	"github.com/hippokampe/api/logger"
 	"github.com/mxschmitt/playwright-go"
+	"github.com/pkg/errors"
 )
 
-func (h *Holberton) login(email, password string) (*models.User, error) {
-	var err error
-	user := &models.User{
-		Email:    email,
-		Password: password,
+func (hbtn *Holberton) Login(credentials models.Login) (models.User, error) {
+	scope := "holberton"
+
+	ctx, err := hbtn.getSession(credentials.Email)
+	if err == nil {
+		return *ctx.User, nil
 	}
 
-	_, err = h.page.Goto(BaseUrl + "/auth/sign_in")
+	ctx, err = hbtn.newSession()
 	if err != nil {
-		logger.Log2(err, "could not goto")
-		return nil, err
+		return models.User{}, errors.Wrap(err, scope)
 	}
 
-	err = h.page.Fill("#user_login", email)
+	user, err := hbtn.login(*ctx.BrowserContext, credentials)
 	if err != nil {
-		logger.Log2(err, "could not set the user[login]")
-		return nil, err
+		return models.User{}, errors.Wrap(err, scope)
 	}
 
-	err = h.page.Fill("#user_password", password)
-	if err != nil {
-		logger.Log2(err, "could not set the user[password]")
-		return nil, err
-	}
-
-	err = h.page.Click("#new_user > div.actions > input", playwright.PageClickOptions{
-		Timeout: playwright.Int(1000 * 5),
-	})
-
-	if err != nil {
-		logger.Log2(err, "could not sent the information")
-		return nil, err
-	}
-
-	_, err = h.page.Goto("https://intranet.hbtn.io/users/my_profile")
-
-	exists, err := h.userExists(h.page, user)
-	if err != nil {
-		logger.Log2(err, "cannot check if user exists")
-		return nil, err
-	}
-
-	if exists {
-		return user, err
-	}
-
-	return nil, nil
+	ctx.User = &user
+	hbtn.addSession(user.Email, ctx)
+	return user, nil
 }
 
-func (h *Holberton) userExists(page *playwright.Page, user *models.User) (bool, error) {
-	exists := false
-	html, _ := page.Content()
-	url := h.setHtml(html, "/login")
-
-	selector := "#user_preferred_name"
-	h.collector.OnHTML(selector, func(div *colly.HTMLElement) {
-		user.Username = cleanString(div.Attr("value"))
-
-		h.InternalStatus.Logged = true
-		exists = true
-	})
-
-	if err := h.collector.Visit(url); err != nil {
-		return false, err
+func (hbtn *Holberton) login(browserCtx playwright.BrowserContext, credentials models.Login) (models.User, error) {
+	scope := "login"
+	page, err := browserCtx.NewPage()
+	if err != nil {
+		return models.User{}, errors.Wrap(err, scope)
 	}
 
-	return exists, nil
+	_, err = page.Goto("https://intranet.hbtn.io/auth/sign_in")
+	if err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	if err := page.Fill("#user_login", credentials.Email); err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	if err := page.Fill("#user_password", credentials.Password); err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	if err := page.Click("#new_user > div.actions > input", playwright.PageClickOptions{
+		Timeout: playwright.Int(1000 * 5),
+	}); err != nil {
+		selectorMsg := `waiting for selector "#new_user > div.actions > input"`
+
+		if !strings.Contains(err.Error(), selectorMsg) {
+			return models.User{}, errors.Wrap(err, scope)
+		}
+
+		return models.User{}, errors.Wrap(ErrBadCredentials, scope)
+	}
+
+	_, err = page.Goto("https://intranet.hbtn.io/users/my_profile")
+	if err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	html, err := page.Content()
+	if err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	url := hbtn.setHtml(html)
+	user, err := hbtn.fillUser(url)
+	if err != nil {
+		return models.User{}, errors.Wrap(err, scope)
+	}
+
+	user.Email = credentials.Email
+	return user, nil
+}
+
+func (hbtn *Holberton) fillUser(url string) (models.User, error) {
+	var user models.User
+
+	hbtn.collector.OnHTML("body", func(body *colly.HTMLElement) {
+		picSelector := "div.row:nth-child(4) > div:nth-child(1) > img:nth-child(1)"
+		firstNameSelector := "#user_first_name"
+		lastNameSelector := "#user_last_name"
+		usernameSelector := "#user_preferred_name"
+		idSelector := ".list-group-item > p:nth-child(3) > code:nth-child(1)"
+		citySelector := "#user_address_city"
+
+		pic := body.DOM.Find(picSelector)
+		firstName := body.DOM.Find(firstNameSelector)
+		lastName := body.DOM.Find(lastNameSelector)
+		username := body.DOM.Find(usernameSelector)
+		id := body.DOM.Find(idSelector)
+		city := body.DOM.Find(citySelector)
+
+		user.ProfileURL, _ = pic.Attr("src")
+		user.FirstName, _ = firstName.Attr("value")
+		user.LastName, _ = lastName.Attr("value")
+		user.UserName, _ = username.Attr("value")
+		user.ID = id.Text()
+		user.City, _ = city.Attr("value")
+	})
+
+	if err := hbtn.collector.Visit(url); err != nil {
+		return models.User{}, ErrServeFile
+	}
+
+	return user, nil
 }
