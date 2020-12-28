e0887412c394e2792da350abf7929236cfe5e94c
Orozco David <dvdizcky@gmail.com>
2020-12-28 07:28:12 -0500

    holberton: session manager for user context added

---
+++ holberton/session.go
@@ -0,0 +1,30 @@
+package holberton
+
+func (hbtn *Holberton) newSession() (*holbertonSession, error) {
+	ctx, err := hbtn.browser.NewContext()
+	if err != nil {
+		return nil, err
+	}
+
+	return &holbertonSession{
+		User:           nil,
+		BrowserContext: &ctx,
+	}, nil
+}
+
+func (hbtn *Holberton) getSession(email string) (*holbertonSession, error) {
+	ctx, ok := hbtn.sessions[email]
+	if !ok {
+		return nil, ErrSessionNotExists
+	}
+
+	return ctx, nil
+}
+
+func (hbtn *Holberton) addSession(email string, ctx *holbertonSession) {
+	hbtn.sessions[email] = ctx
+}
+
+func (hbtn *Holberton) deleteSession(email string) {
+	delete(hbtn.sessions, email)
+}
