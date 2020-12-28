4166b8fa0e60b2a9a438cd2a2ed644f63acfb4aa
Orozco David <dvdizcky@gmail.com>
2020-12-28 07:27:04 -0500

    holberton: constructor refactored and cleaned

--- holberton/index.go
+++ holberton/index.go
@@ -4,89 +4,50 @@ import (
 	"net/http"
 	"net/http/httptest"
 
-	"github.com/hippokampe/configuration/v2/configuration"
-
-	"github.com/hippokampe/api/logger"
-
 	"github.com/gocolly/colly"
-
 	"github.com/mxschmitt/playwright-go"
 )
 
-const (
-	FIREFOX  = "firefox"
-	CHROMIUM = "chromium"
-	WEBKIT   = "webkit"
-)
-
-const (
-	BaseUrl = "https://intranet.hbtn.io"
-)
-
-type Holberton struct {
-	pw             *playwright.Playwright
-	browser        playwright.Browser
-	ts             *httptest.Server
-	mux            *http.ServeMux
-	collector      *colly.Collector
-	page           playwright.Page
-	InternalStatus status
-	Configuration  *configuration.InternalSettings
-}
-
-type status struct {
-	Logged      bool
-	VisitedURLS map[string]bool
-	Started     bool
-	Username    string
-}
-
-func NewSession(browserName string, config *configuration.InternalSettings) (*Holberton, error) {
+func New() (*Holberton, error) {
 	var err error
 
-	holberton := &Holberton{
-		Configuration: config,
+	hbtn := &Holberton{}
+
+	hbtn.pw, err = playwright.Run()
+	if err != nil {
+		return nil, err
 	}
 
 	browserOptions := playwright.BrowserTypeLaunchOptions{
-		Headless: playwright.Bool(true),
+		Headless: playwright.Bool(false),
 	}
 
-	holberton.pw, err = playwright.Run()
+	hbtn.browser, err = hbtn.pw.Firefox.Launch(browserOptions)
 	if err != nil {
 		return nil, err
 	}
 
-	// Selecting browser for scrapping
-	switch browserName {
-	case FIREFOX:
-		holberton.browser, err = holberton.pw.Firefox.Launch(browserOptions)
-	case CHROMIUM:
-		holberton.browser, err = holberton.pw.Chromium.Launch(browserOptions)
-	case WEBKIT:
-		holberton.browser, err = holberton.pw.WebKit.Launch(browserOptions)
-	default:
-		return nil, logger.New("browser not available")
-	}
+	hbtn.mux = http.NewServeMux()
+	hbtn.ts = httptest.NewServer(hbtn.mux)
+	hbtn.collector = colly.NewCollector()
+	hbtn.sessions = make(map[string]*holbertonSession)
 
-	if err != nil {
-		return nil, err
-	}
-
-	return holberton, nil
+	return hbtn, nil
 }
 
-func (h *Holberton) CloseSession() {
+func (hbtn *Holberton) Close() error {
 	var err error
-	if h.browser != nil {
-		if err = h.browser.Close(); err != nil {
-			logger.Log2(err, "could not close browser")
+	if hbtn.browser != nil {
+		if err = hbtn.browser.Close(); err != nil {
+			return err
 		}
 	}
 
-	if h.pw != nil {
-		if err = h.pw.Stop(); err != nil {
-			logger.Log2(err, "could not stop Playwright")
+	if hbtn.pw != nil {
+		if err = hbtn.pw.Stop(); err != nil {
+			return err
 		}
 	}
+
+	return nil
 }
