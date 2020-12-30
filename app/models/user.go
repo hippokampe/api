2f6f0f77b098aad36577f9a38d3c1e3c9d82fca8
Orozco David <dvdizcky@gmail.com>
2020-12-29 19:03:28 -0500

    app: code refactored

--- app/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       string `json:"id"`
-	Email    string `form:"email" json:"email" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-	Username string `json:"username"`
-}
